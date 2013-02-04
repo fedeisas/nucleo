0a344060095a1525baab63692398777db3e42f96
fedeisas <fedeisas@hotmail.com>
2013-02-04 00:13:35 -0300

    deploy

--- application/views/common/css.php
+++ application/views/common/css.php
@@ -2,7 +2,9 @@
 	$this->carabiner->display('css'); 
 	$this->carabiner->display('modernizr'); 
 ?>
+
 <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/images/favicon.ico">
+
 <!-- IE Fix for HTML5 Tags -->
 <!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
