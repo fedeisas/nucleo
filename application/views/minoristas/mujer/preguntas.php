ff0d11f22eeda63b2b3d1521b334adeca0efb38e
Fede Isas <fedeisas@hotmail.com>
2013-02-08 14:38:57 -0300

    deploy

--- application/views/minoristas/mujer/preguntas.php
+++ application/views/minoristas/mujer/preguntas.php
@@ -2,24 +2,45 @@
 
 	<section role="main">
 		<div class="twelve columns">
+
+			<!-- <p>
+				<a href="#" class="button secondary right condiciones-de-compra">
+					<i class="triangle"></i>
+					Condiciones de compra
+				</a>
+				<div class="clearfix"></div>
+			</p> -->
+
+			<div class="panel">
+				<h5>Condiciones de compra</h5>
+				<p>Sólo se realizan compras presentando constancia de CUIT del rubro textil.</p>
+			</div>
+
 			<div class="scroll-pane">
-				<h3 class="question">¿Dónde se puede adquirir los productos de Nucleo?</h3>
-				<p class="answer">Nuestro único punto de venta mayorista es en la calle Concordia 579 Capital Federal, en pleno centro comercial de Flores.</p>
+				<h3 class="question">¿Dónde se pueden adquirir los productos de Nucleo?</h3>
+				<p class="answer">Nuestro único punto de venta mayorista es en la calle Helguera 491 Capital Federal, en pleno centro comercial de Flores.</p>
 				<h3 class="question">¿Cuáles son los horarios de atención?</h3>
-				<p class="answer">Nuestros horarios son de lunes a viernes de 8:00 a 17:30 y sábados de 8:00 a 13:00 hs.</p>
+				<p class="answer">Nuestros horarios son de lunes a viernes de 7:30 a 18:30 y sábados de 8:00 a 13:00.</p>
 				<h3 class="question">¿Cómo es el sistema de venta?</h3>
-				<p class="answer">La cantidad mínima de compra es de 12 prendas.</p>
+				<p class="answer">La cantidad mínima de compra es de 12 prendas. No contamos con lista de precios ya que tenemos una gran cantidad de articulos en stock que se renuevan continuamente.</p>
 				<h3 class="question">¿Cuándo son los lanzamientos de temporada?</h3>
 				<p class="answer">Nuestro año comienza con la temporada Otoño-Invierno en el mes de febrero; Pimavera-Verano, en el mes de agosto.</p>
 				<h3 class="question">¿Ustedes tienen franquicias en el interior del país?</h3>
 				<p class="answer">No por el momento. Es un proyecto en el que estamos trabajando.</p>
 				<h3 class="question">¿Hacen envíos a todo el país?</h3>
-				<p class="answer">Si, según el monto de compra. El envío está a cargo del cliente.</p>
+				<p class="answer">Si, según el monto de compra. No hacemos envíos al exterior, el mismo está a cargo del cliente.</p>
 				<h3 class="question">¿Se puede hacer un pedido por internet?</h3>
-				<p class="answer">No, pero pueden hacerlo telefónicamente al 011 4674 4798</p>
-				<h3 class="question">¿Tienen catálogos de ventas?</h3>
-				<p class="answer">Tenemos catálogos de publicidad con las mismas imágenes que pueden ver por internet.</p>
+				<p class="answer">No, pero pueden hacerlo telefónicamente al 011 4115 0011 a partir de las 16hs.</p>
+				<h3 class="question">¿Tienen catálogos de venta?</h3>
+				<p class="answer">Tenemos catalogos de publicidad con las mismas imágenes que pueden ver por internet, pero son solo un anticipo de lo que encontrarán en el local.</p>
+				<h3 class="question">¿Puedo hacer una compra telefónicamente?</h3>
+				<p class="answer">Las compras telefonicas son UNICAMENTE para reposiciones. La primer compra debe ser realizada personalmente en nuestro local puesto que necesitamos ingresar tus datos en nuestra base de datos.</p>
+				<h3 class="question">Y si vivo en el interior del país, ¿puedo comprar telefónicamente?</h3>
+				<p class="answer">En las primeras compras todos deben acercarse al local, y podrán únicamente hacer reposiciones telefónicamente.</p>
+				<h3 class="question">¿Cómo hago reposiciones por teléfono?</h3>
+				<p class="answer">Detrás de cada prenda hay un código de articulo en el cartón colgante, con ese código se pueden realizar las reposiciones.</p>
 			</div>	
+
 		</div>
 	</section>
 	
